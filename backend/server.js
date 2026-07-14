4c889f3fb30760175cc264a7dd53ed41bdff3331
Sakshi100304 <samrutwarsakshi8@gmail.com>
2026-07-14 14:00:43 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
+app.use(express.static(path.join(__dirname, "../frontend")));
 // ================= PORT =================
 const PORT = process.env.PORT || 5000;
 // ================= DB =================
